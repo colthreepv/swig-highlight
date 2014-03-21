d26c805cd16701972c81456ec75e021e8e84930b
mrgamer <mister.gamer@gmail.com>
2014-03-21 18:29:46 +0100

    Release 0.0.1, initial functionality. All that i need!

---
+++ index.js
@@ -0,0 +1,55 @@
+var hljs = require('highlight.js');
+
+exports.parse = function (str, line, parser, types, options) {
+  parser.on(types.STRING, function (token) {
+    // strip quote or double quote, they _also_ get matched
+    var hlLanguage = token.match.replace(/^("|')|("|')$/g, '');
+    // check if language is supported
+    if (hljs.getLanguage(hlLanguage) !== undefined) {
+      this.out.push(hlLanguage);
+    } else {
+      throw new Error('Unsupported code-highlight "' + hlLanguage + '" on line ' + line + '.');
+    }
+  });
+
+  return true;
+};
+
+exports.compile = function (compiler, args, content, parents, options, blockName) {
+  var highlighted = '',
+      code = '',
+      language,
+      hljsOutput;
+
+  if (args.length > 0) {
+    language = args[0];
+    code = hljs.highlight(language, content[0]).value;
+  } else {
+    hljsOutput = hljs.highlightAuto(content[0]);
+    code = hljsOutput.value;
+    language = hljsOutput.language;
+  }
+
+  highlighted += '<pre><code class="hljs ' + language + '">' + code + '</code></pre>';
+  highlighted = highlighted
+    .replace(/\\/g, '\\\\')
+    .replace(/\n/g, '\\n')
+    .replace(/\r/g, '\\r')
+    .replace(/"/g, '\\"');
+
+  var out = [
+    '(function(){',
+    '  _output += "' + highlighted + '";',
+    '  return _output;',
+    '})();'
+  ].join('\n');
+
+  return out;
+};
+
+exports.ends = true;
+exports.block = true;
+
+exports.apply = function (swig) {
+  swig.setTag('highlight', exports.parse, exports.compile, exports.ends, exports.block);
+};
---
+++ test/index.js
@@ -0,0 +1,37 @@
+/* global describe:false, it:false, before: false */
+var assert = require('assert'),
+    path = require('path'),
+    swig = require('swig');
+
+require('../index').apply(swig);
+
+describe('swig-highlight', function () {
+  var template, compiledTemplate;
+  before(function () {
+    template = swig.compileFile(path.resolve(process.cwd(), 'test/fixtures/html.swig'));
+    compiledTemplate = template();
+  });
+
+  it('should add a lot of tags with .hljs class', function () {
+    var regex = /class="hljs/;
+    assert.ok(regex.test(compiledTemplate));
+  });
+
+  it('should have "bash" class inside code tag', function () {
+    var regex = /class="hljs bash/;
+    assert.ok(regex.test(compiledTemplate));
+  });
+});
+
+describe('guess language', function () {
+  var template, compiledTemplate;
+  before(function () {
+    template = swig.compileFile(path.resolve(process.cwd(), 'test/fixtures/auto.swig'));
+    compiledTemplate = template();
+  });
+
+  it('should have "python" as guessed language', function () {
+    var regex = /class="hljs python/;
+    assert.ok(regex.test(compiledTemplate));
+  });
+});
